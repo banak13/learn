9ea56037836450c9a4c190b16f7ba977fa05f9d6
Pawel Banak <banak13@o2.pl>
2020-08-25 08:16:03 +0200

    initial

---
+++ liczby.py
@@ -0,0 +1,16 @@
+number = []
+
+for value in range(1,100):
+    if value % 5 == 0:
+        number.append(value)
+
+print(number)
+
+number2 = []
+
+for item in number:
+    number2.append(item**3)
+print(number2)
+
+
+asadvfgb
---
+++ lista_zakupow.py
@@ -0,0 +1,17 @@
+print("LISTA ZAKUPOW")
+
+lista_zakupów = {
+    "piekarnii": ["chleb", "bułki", "pączek"],
+    "warzywniaka": ["marchew", "seler", "rukola"]
+}
+
+for sklep, rzeczy in lista_zakupów.items():
+    rzeczy_tekst = ''
+    for produkt in rzeczy:
+        rzeczy_tekst += " {},".format(produkt.capitalize())
+    print("Idę do {} i kupuje tam{}".format(sklep.capitalize(), rzeczy_tekst))
+    x = 0
+
+    for key in lista_zakupów.values():
+        x += len(key)
+print("W sumie kupuję {} produktów.".format(x))
