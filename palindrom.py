74279d2b439f45103fa7012072295b97c2cd5c37
Pawel Banak <banak13@o2.pl>
2020-08-27 15:01:33 +0200

    1.zmiana kodu

--- palindrom.py
+++ palindrom.py
@@ -1,8 +1,9 @@
 def is_palindrome(s):
-  i = len(s)/2
-  for i in range (len(s)//2):
-    if s[i] != s[-i-1]: return False
-  return True
+
+  return list(s) == list(s)[::-1]
+
+
+
 
 print(is_palindrome("kajak"))
 
